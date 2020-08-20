5c5c7879b55e97b36959c75ba3947de4bacf275a
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-20 13:59:06 -0400

    Add another list

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,15 @@ import { AppContainer } from './styles';
 import { Column } from './Column';
 import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
-
-const Button = styled.button`
-  background-color: linear-gradient(#333, #fff);
-`;
+import { useAppState } from './AppStateContext';
 
 export const App = () => {
+  const { state } = useAppState();
   return (
     <AppContainer>
-      <Column text='To Do'>
-        <Card text='Generate app scaffold' />
-      </Column>
-      <Column text='In Progress'>
-        <Card text='Learn Typescript' />
-        <Card text='Learn Elasticsearch' />
-        <Card text='Learn Java' />
-        <Card text='Learn Rust' />
-        <Card text='Learn React' />
-      </Column>
-      <Column text='Done'>
-        <Card text='Begin to static typing' />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
     </AppContainer>
   );
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AddNewItem } from './AddNewItem';
 import { ColumnContainer, ColumnTitle } from './styles';
+import { useAppState } from './AppStateContext';
+import { Card } from './Card';
 
 interface ColumnProps {
   text: string;
+  index: number;
 }
 
 type PropsWithChildren<P> = P & {
   children?: React.ReactNode;
 };
 
-export const Column = ({
-  text,
-  children,
-}: React.PropsWithChildren<ColumnProps>) => {
+export const Column = ({ text, index }: ColumnProps) => {
+  const { state } = useAppState();
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
       <AddNewItem
         toggleButtonText='+ Add another task'
         onAdd={console.log}
